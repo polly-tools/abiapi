870569479dcac33deadbb865576cf34464a90777
Troels Abrahamsen <troels@blck.dk>
2022-05-10 15:33:17 +0200

    Add lodash + parsers.js for global parser methods

---
+++ parsers.js
@@ -0,0 +1,28 @@
+import {isArray, isObject} from 'lodash';
+
+function bigNumbersToNumber(value){
+
+    if(value._isBigNumber){
+        return value.toNumber();
+    }
+    else if(isArray(value)){
+        return value.map(bigNumbersToNumber);
+    }
+    else if(isObject(value)){
+
+        for(const key in value) {
+            if (Object.hasOwnProperty.call(value, key)) {
+                value[key] = bigNumbersToNumber(value[key])
+            }
+        }    
+        return value;
+    }
+
+    return value;
+
+}
+
+
+module.exports = {
+    bigNumbersToNumber
+}
